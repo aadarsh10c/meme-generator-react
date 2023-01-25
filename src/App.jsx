335b5da4faf737d55a32f95ab3e32f52b685db58
Adarsh Kumar <adarsh010c@gmail.com>
2023-01-25 15:53:20 +0530

    Added files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react' ;
+import Header from './component/Header';
+import Content from './component/Content';
+
+export default function App () {
+  return (
+    <div className="container">
+      <Header />
+      <Content />
+    </div>
+  );
+}
---
+++ src/component/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+export default function Header (){
+    return(
+        <header className="header">
+            <div className="header__main">
+                <img src="" alt="" className="header__main-img" />
+                <h3 className="header__main-title">
+                    Meme Generator
+                </h3>
+            </div>
+            <h5 className="header__sub">
+                React Course Project-3
+            </h5>
+        </header>
+    );
+}
